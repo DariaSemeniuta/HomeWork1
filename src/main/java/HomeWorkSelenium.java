fb44c803d3cfa9eeaba0c08fa25a1c845c90f046
Daria Semeniuta <dashasemenyuta@gmail.com>
2016-04-11 22:52:12 +0300

    Initial commit

---
+++ src/main/java/HomeWorkSelenium.java
@@ -0,0 +1,57 @@
+import junit.framework.TestCase;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Daria
+ */
+public class HomeWorkSelenium extends TestCase {
+    private static WebDriver driver= new FirefoxDriver();
+    private String firsPage="https://en.wikipedia.org/wiki/Main_Page";
+    private By searchId = By.id("searchInput");
+    WebElement search;
+    @BeforeClass
+    public void setUp() throws Exception{
+        driver.get(firsPage);
+    }
+
+    @Test
+    public void testFruit() throws Exception {
+        try {
+            search = driver.findElement(searchId);
+            search.sendKeys("Fruit");
+            search.submit();
+            WebElement fruit = driver.findElement(By.linkText("apple"));
+            fruit.click();
+            assertNotNull(driver.findElement(By.linkText("fruit tree")));
+            WebElement element = driver.findElement(By.className("mw-headline"));
+            assertEquals(element.getText(), "Botanical information");
+        } catch (NoSuchElementException e) {
+            System.out.println("Элемент не найден!");
+        }
+    }
+    @Test
+    public void testTomato() throws Exception{
+        try {
+            search = driver.findElement(searchId);
+            search.sendKeys("Tomato");
+            search.submit();
+            assertNotNull(driver.findElement(By.xpath(".//a[text() = 'vegetable']")));
+            assertEquals(driver.getTitle(), "Tomato - Wikipedia, the free encyclopedia");
+        }
+        catch (NoSuchElementException e) {
+            System.out.println("Элемент не найден!");
+        }
+    }
+    @AfterClass
+    public void afterClass() throws Exception { 
+        System.out.println( "Тест окончен" );
+        driver.quit();
+    }
+}
